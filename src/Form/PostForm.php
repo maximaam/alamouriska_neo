79932dc8f1f2dce148f0d0b166c99e92114480f8
Maamar <mimoberlino@gmail.com>
2025-06-02 15:28:52 +0200

    Added rector

--- src/Form/PostForm.php
+++ src/Form/PostForm.php
@@ -31,7 +31,7 @@ class PostForm extends AbstractType
                 'required' => false,
                 'allow_delete' => true,
             ])
-            ->addEventListener(FormEvents::SUBMIT, static function (FormEvent $event) {
+            ->addEventListener(FormEvents::SUBMIT, static function (FormEvent $event): void {
                 $post = $event->getData();
                 if (\in_array($post->getType(), [PostType::proverb->value, PostType::joke->value], true)) {
                     $post->setTitle(mb_strimwidth($post->getDescription(), 0, 100, '...'));
