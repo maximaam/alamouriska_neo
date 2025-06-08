98ed6e1b1b398283023c7c2454a1b89c265910ac
Maamar <mimoberlino@gmail.com>
2025-06-08 19:13:36 +0200

    title validators

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,19 +4,29 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/profile', name: 'app_profile_', priority: 3)]
 final class ProfileController extends AbstractController
 {
     #[Route('/{pseudo}', name: 'index')]
-    public function index(UserRepository $userRepository, string $pseudo): Response
+    public function index(UserRepository $userRepository, PostRepository $postRepository, string $pseudo): Response
     {
+        if (null === $user = $userRepository->findOneBy(['pseudo' => $pseudo])) {
+            throw new NotFoundHttpException('profile_not_found');
+        }
+
+        $posts = $postRepository->findBy(['user' => $user]);
+
         return $this->render('profile/index.html.twig', [
-            'user' => $userRepository->findOneBy(['pseudo' => $pseudo]),
+            'user' => $user,
+            'posts' => $posts,
+            'posts_count' => count($posts),
         ]);
     }
 }
